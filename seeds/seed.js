19baf004f7833527ac9cf32ef6d4a9554066b7a0
Jung David Lee <jungdavidlee@gmail.com>
2021-03-30 19:40:50 -0700

    npm i and updated .env and updated seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,17 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Animal, Cart } = require('../models');
 
 const userData = require('./userData.json');
-const exoticsData = require('./exoticsData.json');
-const frogsData = require('./frogs.json');
-const lizardsData = require('./lizards.json');
-const snakesData = require('./snakes.json');
-const turtlesData = require('./turtles.json');
+const animalData = require('./animal.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
-    returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const animals = await Animal.bulkCreate(animalData);
 
   process.exit(0);
 };
