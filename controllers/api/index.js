2c976b7e6724e397800187bb39e0a122c8ecc2e2
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-03 10:14:27 -0700

    testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const cartRoutes = require('./cartRoutes');
+const cartRoutes = require('./CartRoutes');
 
 router.use('/users', userRoutes);
 
